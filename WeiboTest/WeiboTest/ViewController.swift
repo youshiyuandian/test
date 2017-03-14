b8e7fc5c7c9e5d8d10985c2de41da0feb1f19c61
zuxi li <ios_lizuxi@163.com>
2017-03-14 15:39:55 +0800

    111111

---
+++ WeiboTest/WeiboTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeiboTest
+//
+//  Created by zuxi li on 2017/3/14.
+//  Copyright © 2017年 lingtu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
